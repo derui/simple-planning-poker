ea300c86ecab01e05376c1d7729c0391de757b09
derui <derutakayu@gmail.com>
2021-08-21 14:53:43 +0900

    feat: allow to switch firebase configuration

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -7,4 +7,9 @@ import { firebaseConfig } from "./firebase.config";
 
 firebase.initializeApp(firebaseConfig);
 
+const database = firebase.database();
+if (location.hostname === "localhost") {
+  database.useEmulator("localhost", 9000);
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
