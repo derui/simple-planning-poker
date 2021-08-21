57f5404fb8c987eb9b1dd669f5f631c2f9637476
derui <derutakayu@gmail.com>
2021-08-21 14:42:00 +0900

    feat: add firebase configuration

---
+++ src/ts/firebase.config.prod.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCnapCA2OfLAgaB3myLpB6W04-Z_JrcuFg",
+  authDomain: "planning-poker-67f2e.firebaseapp.com",
+  projectId: "planning-poker-67f2e",
+  storageBucket: "planning-poker-67f2e.appspot.com",
+  messagingSenderId: "101864689952",
+  databaseURL: "https://planning-poker-67f2e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  appId: "1:101864689952:web:26999c669efd3e007e4745",
+};
--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,5 +1,10 @@
+import firebase from "firebase/app";
+import "firebase/database";
 import React from "react";
 import * as ReactDOM from "react-dom";
 import { App } from "./app";
+import { firebaseConfig } from "./firebase.config";
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById("root"));
