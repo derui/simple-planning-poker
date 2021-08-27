38cee6129237d4796cd7c5a8c0f6b774b2420b15
derui <derutakayu@gmail.com>
2021-08-27 13:41:06 +0900

    feat: allow changing user name in all games joined

---
+++ src/ts/infrastractures/event/user-name-changed-event-listener.ts
@@ -0,0 +1,31 @@
+import firebase from "firebase";
+import { DefinedDomainEvents, DOMAIN_EVENTS } from "@/domains/event";
+import { DomainEventListener } from "./domain-event-listener";
+
+export class UserNameChangedEventListener implements DomainEventListener {
+  constructor(private database: firebase.database.Database) {}
+
+  handle(event: DefinedDomainEvents): void {
+    if (event.kind == DOMAIN_EVENTS.UserNameChanged) {
+      this.database
+        .ref(`users/${event.userId}`)
+        .once("value")
+        .then((ref) => {
+          const user = ref.val();
+          if (!user) {
+            return;
+          }
+
+          const games = user["joinedGames"] || {};
+          const updates: { [key: string]: any } = {};
+          Object.keys(games).forEach((key) => {
+            updates[`games/${key}/joinedUser/${event.userId}`] = event.name;
+          });
+
+          this.database.ref().update(updates);
+        });
+    }
+
+    return;
+  }
+}
