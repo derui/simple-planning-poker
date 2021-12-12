7876c7c6a9f0b9b07211ce82fdeabae0c5042362
derui <derutakayu@gmail.com>
2021-12-12 12:37:42 +0900

    test: split test into other module

--- src/rust/domains/card.rs
+++ src/rust/domains/card.rs
@@ -1,4 +1,7 @@
-use crate::domains::story_point::StoryPoint;
+use super::story_point::StoryPoint;
+
+#[cfg(test)]
+mod tests;
 
 #[derive(Debug, PartialEq, Clone)]
 enum CardInner {
@@ -23,34 +26,5 @@ impl Card {
             CardInner::GiveUpCard => None,
             CardInner::StoryPointCard(s) => Some(s),
         }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn should_return_none_if_giveup_card() {
-        // arrange
-        let v = Card::giveup();
-
-        // do
-        let actual = v.as_story_point();
-
-        // arrange
-        assert_eq!(actual, None)
-    }
-
-    #[test]
-    fn should_return_point_if_storypoint_card() {
-        // arrange
-        let v = Card::storypoint(StoryPoint::new(4));
-
-        // do
-        let actual = v.as_story_point();
-
-        // arrange
-        assert_eq!(actual, Some(StoryPoint::new(4)))
     }
 }
--- src/rust/domains/story_point.rs
+++ src/rust/domains/story_point.rs
@@ -1,5 +1,8 @@
 use std::ops::Add;
 
+#[cfg(test)]
+mod tests;
+
 #[derive(Debug, PartialEq, Copy)]
 pub struct StoryPoint(u32);
 
@@ -24,48 +27,5 @@ impl StoryPoint {
 
     pub fn as_u32(&self) -> u32 {
         self.0
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn add_point() {
-        // arrange
-        let v1 = StoryPoint(3);
-        let v2 = StoryPoint(4);
-
-        // do
-        let actual = v1 + v2;
-
-        // verify
-        assert_eq!(actual, StoryPoint(7))
-    }
-
-    #[test]
-    fn allow_to_convert_u32() {
-        // arrange
-        let v = StoryPoint(3);
-
-        // do
-        let actual = v.as_u32();
-
-        // verify
-        assert_eq!(actual, 3)
-    }
-
-    #[test]
-    fn allow_copy() {
-        // arrange
-        let v = StoryPoint(3);
-        let reference = &v;
-
-        // do
-        let v2 = Some(*reference);
-
-        // verify
-        assert_eq!(v2, Some(StoryPoint(3)))
     }
 }
