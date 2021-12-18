3e2ec62f8a99adbb0cb97c780210cacf2177339c
derui <derutakayu@gmail.com>
2021-12-18 14:31:46 +0900

    feat: port game domain

--- src/rust/planning_poker/src/domains/game.rs
+++ src/rust/planning_poker/src/domains/game.rs
@@ -1,10 +1,176 @@
+use std::{ops::RangeBounds, vec};
+
 use domain_macro::DomainId;
 use uuid::Uuid;
 
-use super::id::{DomainId, Id};
+use super::{
+    base::Entity,
+    card::Card,
+    event::DomainEventKind,
+    game_player::GamePlayerId,
+    id::{DomainId, Id},
+    invitation::Invitation,
+    selectable_cards::SelectableCards,
+    story_point::StoryPoint,
+};
 
 #[cfg(test)]
 mod tests;
 
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub struct AveragePoint(f32);
+
+impl AveragePoint {
+    fn as_f32(&self) -> f32 {
+        self.0
+    }
+}
+
 #[derive(PartialEq, Debug, DomainId, Clone, Copy)]
 pub struct GameId(Id<Uuid>);
+
+#[derive(PartialEq, Debug, Clone)]
+struct PlayerHand {
+    player: GamePlayerId,
+    card: Card,
+}
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Game {
+    id: GameId,
+    name: String,
+    _showed_down: bool,
+    players: Vec<GamePlayerId>,
+    cards: SelectableCards,
+    hands: Vec<PlayerHand>,
+}
+
+impl Entity<GameId> for Game {
+    fn id(&self) -> GameId {
+        self.id
+    }
+}
+
+impl Game {
+    pub fn new(
+        id: GameId,
+        name: String,
+        showed_down: bool,
+        players: &Vec<GamePlayerId>,
+        cards: &SelectableCards,
+    ) -> Self {
+        if players.is_empty() {
+            panic!("Least one players need");
+        }
+
+        Self {
+            id,
+            name,
+            _showed_down: showed_down,
+            players: players.clone(),
+            cards: cards.clone(),
+            hands: vec![],
+        }
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn showed_down(&self) -> bool {
+        self._showed_down
+    }
+
+    pub fn players(&self) -> &Vec<GamePlayerId> {
+        &self.players
+    }
+
+    pub fn cards(&self) -> &SelectableCards {
+        &self.cards
+    }
+
+    pub fn make_invitation(&self) -> Invitation {
+        Invitation::new(self.id)
+    }
+
+    pub fn can_show_down(&self) -> bool {
+        !self.hands.is_empty() && !self._showed_down
+    }
+
+    pub fn show_down<F>(&mut self, mut receiver: F)
+    where
+        F: FnMut(DomainEventKind) + 'static,
+    {
+        if !self.can_show_down() {
+            return;
+        }
+
+        self._showed_down = true;
+        receiver(DomainEventKind::GameShowedDown { game_id: self.id })
+    }
+
+    pub fn calculate_average(&self) -> Result<AveragePoint, String> {
+        if !self.showed_down() {
+            return Err(String::from("can not calculate if did not show down yet"));
+        }
+
+        let hands = self
+            .hands
+            .iter()
+            .filter_map(|v| v.card.as_story_point())
+            .collect::<Vec<StoryPoint>>();
+
+        if hands.len() == 0 {
+            return Ok(AveragePoint(0.0));
+        }
+
+        let average = hands.iter().fold(0, |accum, v| accum + v.as_u32());
+        let average = average as f32 / hands.len() as f32;
+
+        Ok(AveragePoint(average))
+    }
+
+    pub fn change_name(&mut self, name: &String) {
+        if name.len() == 0 {
+            panic!("Can not change name to empty");
+        }
+
+        self.name = name.clone()
+    }
+
+    pub fn can_change_name(name: &String) -> bool {
+        name.len() == 0
+    }
+
+    pub fn next_game<F>(&mut self, mut receiver: F)
+    where
+        F: FnMut(DomainEventKind) + 'static,
+    {
+        self._showed_down = false;
+
+        receiver(DomainEventKind::NewGameStarted { game_id: self.id })
+    }
+
+    pub fn give_player_hand(&mut self, player_id: GamePlayerId, card: &Card) {
+        if !self.players.contains(&player_id) {
+            panic!("Can not give player")
+        }
+
+        let hands = self
+            .hands
+            .iter()
+            .map(|v| {
+                if player_id == v.player {
+                    PlayerHand {
+                        card: card.clone(),
+                        ..v.clone()
+                    }
+                } else {
+                    v.clone()
+                }
+            })
+            .collect();
+
+        self.hands = hands;
+    }
+}
--- src/rust/planning_poker/src/domains/game/tests.rs
+++ src/rust/planning_poker/src/domains/game/tests.rs
@@ -1,4 +1,9 @@
-use crate::domains::{game::GameId, id::Id};
+use crate::domains::{
+    game::{Game, GameId},
+    id::Id,
+    selectable_cards::SelectableCards,
+    story_point::StoryPoint,
+};
 use uuid::Uuid;
 
 use crate::utils::uuid_factory::UuidFactory;
@@ -14,4 +19,61 @@ fn allow_create_id_with_uuid() {
 
     // verify
     assert_eq!(id.to_string(), "936da01f-9abd-4d9d-80c7-02af85c822a8");
+}
+
+#[test]
+#[should_panic]
+fn create_with_zero_player() {
+    // arrange
+    let game_id = Id::create::<GameId>(&UuidFactory::default());
+    let name = String::from("value");
+    let cards = SelectableCards::new(&vec![StoryPoint::new(1)]);
+
+    // do
+    Game::new(game_id, name, true, &vec![], &cards);
+
+    // verify
+}
+
+#[test]
+fn can_not_average_when_no_showed_down() {
+    // arrange
+    let game_id = Id::create::<GameId>(&UuidFactory::default());
+    let name = String::from("value");
+    let cards = SelectableCards::new(&vec![StoryPoint::new(1)]);
+    let game = Game::new(
+        game_id,
+        name,
+        false,
+        &vec![Id::create(&UuidFactory::default())],
+        &cards,
+    );
+
+    // do
+    let v = game.calculate_average();
+
+    // verify
+    assert_eq!(game.showed_down(), false);
+    assert_eq!(v.is_err(), true);
+}
+
+#[test]
+fn can_not_average_when_no_hands() {
+    // arrange
+    let game_id = Id::create::<GameId>(&UuidFactory::default());
+    let name = String::from("value");
+    let cards = SelectableCards::new(&vec![StoryPoint::new(1)]);
+    let game = Game::new(
+        game_id,
+        name,
+        false,
+        &vec![Id::create(&UuidFactory::default())],
+        &cards,
+    );
+
+    // do
+    let v = game.calculate_average();
+
+    // verify
+    assert_eq!(v.is_err(), true);
 }
