6702692ff3f1efa33378768559e7eff9b9bd2479
derui <derutakayu@gmail.com>
2021-12-19 09:52:41 +0900

    feat: port user

--- src/rust/planning_poker/src/domains/user.rs
+++ src/rust/planning_poker/src/domains/user.rs
@@ -1,7 +1,97 @@
 use domain_macro::DomainId;
 use uuid::Uuid;
 
-use super::id::{DomainId, Id};
+use super::{
+    event::DomainEventKind,
+    game::GameId,
+    game_player::GamePlayerId,
+    id::{DomainId, Id},
+};
+
+#[cfg(test)]
+mod tests;
 
 #[derive(PartialEq, Debug, DomainId, Clone, Copy)]
 pub struct UserId(Id<Uuid>);
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct JoinedGame {
+    game: GameId,
+    game_player: GamePlayerId,
+}
+
+pub struct User {
+    id: UserId,
+    name: String,
+    joined_games: Vec<JoinedGame>,
+}
+
+impl User {
+    pub fn new(id: UserId, name: &str, joined_games: &[JoinedGame]) -> Self {
+        if !Self::can_change_name(name) {
+            panic!("Can not use empty name");
+        }
+
+        Self {
+            id,
+            name: String::from(name),
+            joined_games: Vec::from(joined_games),
+        }
+    }
+
+    pub fn id(&self) -> UserId {
+        self.id
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn can_change_name(name: &str) -> bool {
+        name.len() > 0
+    }
+
+    pub fn change_name(&mut self, name: &str) {
+        if !Self::can_change_name(name) {
+            panic!("Can not change name")
+        }
+
+        self.name = String::from(name)
+    }
+
+    pub fn is_joined(&self, game_id: GameId) -> bool {
+        self.find_joined_game(game_id).is_some()
+    }
+
+    pub fn find_joined_game(&self, game_id: GameId) -> Option<JoinedGame> {
+        self.joined_games
+            .iter()
+            .find(|v| v.game == game_id)
+            .map(|v| v.clone())
+    }
+
+    pub fn leave_from<F>(&mut self, game_id: GameId, mut receiver: F)
+    where
+        F: FnMut(DomainEventKind) + 'static,
+    {
+        if let Some(game) = self.find_joined_game(game_id) {
+            let new_games = self
+                .joined_games
+                .iter()
+                .filter_map(|v| {
+                    if v.game != game.game {
+                        Some(v.clone())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<JoinedGame>>();
+            self.joined_games = new_games;
+
+            receiver(DomainEventKind::UserLeavedFromGame {
+                game_id: game.game,
+                game_player_id: game.game_player,
+            })
+        }
+    }
+}
---
+++ src/rust/planning_poker/src/domains/user/tests.rs
@@ -0,0 +1,170 @@
+use crate::{domains::id::Id, utils::uuid_factory::UuidFactory};
+
+use super::*;
+
+#[test]
+#[should_panic]
+fn can_not_create_with_empty_name() {
+    // arrange
+    let id = Id::create(&UuidFactory::default());
+
+    // do
+    User::new(id, "", &[]);
+
+    // verify
+}
+
+#[test]
+fn create_user() {
+    // arrange
+    let id = Id::create(&UuidFactory::default());
+
+    // do
+    let user = User::new(id, "name", &[]);
+
+    // verify
+    assert_eq!(user.id(), id);
+    assert_eq!(user.name(), "name")
+}
+
+#[test]
+fn change_name() {
+    // arrange
+    let id = Id::create(&UuidFactory::default());
+    let mut user = User::new(id, "name", &[]);
+
+    // do
+    user.change_name("name2");
+
+    // verify
+    assert_eq!(user.name(), "name2")
+}
+
+#[test]
+#[should_panic]
+fn can_not_change_name() {
+    // arrange
+    let id = Id::create(&UuidFactory::default());
+    let mut user = User::new(id, "name", &[]);
+
+    // do
+    user.change_name("");
+
+    // verify
+}
+
+#[test]
+fn can_change_name() {
+    // arrange
+
+    // do
+
+    // verify
+    assert_eq!(User::can_change_name(""), false);
+    assert_eq!(User::can_change_name("a"), true);
+}
+
+#[cfg(test)]
+mod joined_game {
+    use crate::domains::user::JoinedGame;
+
+    use super::*;
+
+    #[test]
+    fn is_joined_for_joined_game() {
+        // arrange
+        let id = Id::create(&UuidFactory::default());
+        let game_id = Id::create(&UuidFactory::default());
+        let player_id = Id::create(&UuidFactory::default());
+        let user = User::new(
+            id,
+            "name",
+            &[JoinedGame {
+                game: game_id,
+                game_player: player_id,
+            }],
+        );
+
+        // do
+        let ret = user.is_joined(game_id);
+
+        // verify
+        assert_eq!(ret, true)
+    }
+
+    #[test]
+    fn is_joined_for_not_joined_game() {
+        // arrange
+        let id = Id::create(&UuidFactory::default());
+        let game_id = Id::create(&UuidFactory::default());
+        let user = User::new(id, "name", &[]);
+
+        // do
+        let ret = user.is_joined(game_id);
+
+        // verify
+        assert_eq!(ret, false)
+    }
+
+    #[test]
+    fn find_joined_game() {
+        // arrange
+        let id = Id::create(&UuidFactory::default());
+        let game_id = Id::create(&UuidFactory::default());
+        let player_id = Id::create(&UuidFactory::default());
+        let joined_game = JoinedGame {
+            game: game_id,
+            game_player: player_id,
+        };
+        let user = User::new(id, "name", &[joined_game.clone()]);
+
+        // do
+        let ret = user.find_joined_game(game_id);
+
+        // verify
+        assert_eq!(ret, Some(joined_game))
+    }
+}
+
+mod leave_game {
+    use super::*;
+
+    #[test]
+    fn leave_from_joined_game() {
+        // arrange
+        let id = Id::create(&UuidFactory::default());
+        let game_id = Id::create(&UuidFactory::default());
+        let player_id = Id::create(&UuidFactory::default());
+        let joined_game = JoinedGame {
+            game: game_id,
+            game_player: player_id,
+        };
+        let mut user = User::new(id, "name", &[joined_game.clone()]);
+
+        // do
+        user.leave_from(game_id, move |e| {
+            assert_eq!(
+                e,
+                DomainEventKind::UserLeavedFromGame {
+                    game_id,
+                    game_player_id: player_id
+                }
+            )
+        });
+
+        // verify
+    }
+
+    #[test]
+    fn leave_from_not_joined_game() {
+        // arrange
+        let id = Id::create(&UuidFactory::default());
+        let game_id = Id::create(&UuidFactory::default());
+        let mut user = User::new(id, "name", &[]);
+
+        // do
+        user.leave_from(game_id, move |_| panic!("do not call receiver"));
+
+        // verify
+    }
+}
