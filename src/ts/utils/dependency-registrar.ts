f71cb8403144675f5f18592bbd4e938d213cd842
derui <derutakayu@gmail.com>
2022-04-03 09:36:26 +0900

    feat: implement very simple dependency registrar

---
+++ src/ts/utils/dependency-registrar.ts
@@ -0,0 +1,50 @@
+type Phantom<T> = {
+  __phantom: never;
+  _holder: T | undefined;
+};
+
+type Bean<T> = {
+  name: string;
+  bean: T;
+  type: Phantom<T>;
+};
+
+export interface DependencyRegistrar<S = { [k: string]: any }> {
+  register<T>(name: keyof S, bean: T): void;
+
+  resolve<T = S[keyof S]>(name: keyof S): T;
+}
+
+class DependencyRegistrarImpl<S> implements DependencyRegistrar<S> {
+  constructor(private beans: Bean<any>[] = []) {}
+
+  register<T>(name: keyof S, bean: T) {
+    const registeredBean = this.beans.find((v) => v.name === name);
+
+    if (registeredBean) {
+      return;
+    }
+
+    this.beans.push({
+      name: name as string,
+      bean,
+      type: {
+        _holder: undefined,
+      } as Phantom<T>,
+    });
+  }
+
+  resolve<T = S[keyof S]>(name: keyof S): T {
+    const bean = this.beans.find((v) => v.name === name)?.bean;
+
+    if (!bean) {
+      throw Error(`Not found bean that is name of ${name}`);
+    }
+
+    return bean as T;
+  }
+}
+
+export const createDependencyRegistrar: <T>() => DependencyRegistrar<T> = () => {
+  return new DependencyRegistrarImpl();
+};
