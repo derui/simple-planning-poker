a962c1a14adf94c5e9f1164fbd291e9a4c315ba3
derui <derutakayu@gmail.com>
2022-04-10 08:58:31 +0900

    refactor: split action

---
+++ src/ts/status/game/actions/use-join-user.ts
@@ -0,0 +1,46 @@
+import { useRecoilCallback, useRecoilValue } from "recoil";
+import { GameId } from "@/domains/game";
+import { InvitationSignature } from "@/domains/invitation";
+import { DependencyRegistrar } from "@/utils/dependency-registrar";
+import { Dependencies } from "@/dependencies";
+import currentUserState from "@/status/signin/atoms/current-user";
+import currentGamePlayerState from "../atoms/current-game-player-state";
+import { gamePlayerToViewModel } from "../dxo";
+
+export default function createUseJoinUser(registrar: DependencyRegistrar<Dependencies>) {
+  const gameRepository = registrar.resolve("gameRepository");
+  const gamePlayerRepository = registrar.resolve("gamePlayerRepository");
+  const joinUserUseCase = registrar.resolve("joinUserUseCase");
+
+  return () => {
+    const currentUser = useRecoilValue(currentUserState);
+
+    return useRecoilCallback(({ set }) => async (signature: InvitationSignature, callback: (id: GameId) => void) => {
+      if (!currentUser.id) {
+        return;
+      }
+
+      const ret = await joinUserUseCase.execute({
+        userId: currentUser.id,
+        signature,
+      });
+
+      if (ret.kind === "success") {
+        const gamePlayer = await gamePlayerRepository.findBy(ret.gamePlayerId);
+        set(currentGamePlayerState, (prev) => {
+          return gamePlayer ? gamePlayerToViewModel(gamePlayer, currentUser.name) : undefined || prev;
+        });
+
+        if (!gamePlayer) {
+          return;
+        }
+
+        const game = await gameRepository.findBy(gamePlayer.game);
+
+        if (game) {
+          callback(game.id);
+        }
+      }
+    });
+  };
+}
