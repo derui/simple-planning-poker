ee2213c809927b9ef3a7b7b95665a852a29f917a
derui <derutakayu@gmail.com>
2023-02-20 21:54:10 +0900

    test: use vitest

--- src/infrastractures/card-converter.test.ts
+++ src/infrastractures/card-converter.test.ts
@@ -1,29 +1,26 @@
+import { test, expect } from "vitest";
 import { createGiveUpCard, createStoryPointCard } from "@/domains/card";
 import { createStoryPoint } from "@/domains/story-point";
 import { deserializeCard, serializeCard } from "./card-converter";
 
-describe("infrastracture", () => {
-  describe("card converter", () => {
-    test("serialize and deserialize give up card", () => {
-      // Arrange
-      const card = createGiveUpCard();
+test("serialize and deserialize give up card", () => {
+  // Arrange
+  const card = createGiveUpCard();
 
-      // Act
-      const ret = deserializeCard(serializeCard(card));
+  // Act
+  const ret = deserializeCard(serializeCard(card));
 
-      // Assert
-      expect(ret).toEqual(card);
-    });
+  // Assert
+  expect(ret).toEqual(card);
+});
 
-    test("serialize and deserialize give up card", () => {
-      // Arrange
-      const card = createStoryPointCard(createStoryPoint(3));
+test("serialize and deserialize give up card", () => {
+  // Arrange
+  const card = createStoryPointCard(createStoryPoint(3));
 
-      // Act
-      const ret = deserializeCard(serializeCard(card));
+  // Act
+  const ret = deserializeCard(serializeCard(card));
 
-      // Assert
-      expect(ret).toEqual(card);
-    });
-  });
+  // Assert
+  expect(ret).toEqual(card);
 });
--- src/infrastractures/game-player-repository.test.ts
+++ src/infrastractures/game-player-repository.test.ts
@@ -1,3 +1,4 @@
+import { test, expect, beforeAll, afterAll, afterEach } from "vitest";
 import { createGame, createGameId } from "@/domains/game";
 import { createGamePlayer, createGamePlayerId } from "@/domains/game-player";
 import { createSelectableCards } from "@/domains/selectable-cards";
@@ -7,99 +8,95 @@ import { initializeTestEnvironment, RulesTestEnvironment } from "@firebase/rules
 import { GamePlayerRepositoryImpl } from "./game-player-repository";
 import { GameRepositoryImpl } from "./game-repository";
 
-describe("infrastructure", () => {
-  describe("game player repository", () => {
-    let database: any;
-    let testEnv: RulesTestEnvironment;
+let database: any;
+let testEnv: RulesTestEnvironment;
 
-    beforeAll(async () => {
-      testEnv = await initializeTestEnvironment({
-        projectId: "demo-project-1234",
-        database: {
-          host: "localhost",
-          port: 9000,
-        },
-      });
-      database = testEnv.authenticatedContext("alice").database();
-    });
-
-    afterAll(async () => {
-      testEnv.cleanup();
-    });
-
-    afterEach(async () => {
-      await testEnv.clearDatabase();
-    });
-
-    test("should be able to save and find a game player", async () => {
-      // Arrange
-      const game = createGame({
-        id: createGameId(),
-        name: "test",
-        players: [createGamePlayerId()],
-        cards: createSelectableCards([1, 2].map(createStoryPoint)),
-      });
-
-      const player = createGamePlayer({
-        id: game.players[0],
-        userId: createUserId(),
-        gameId: game.id,
-        cards: game.cards,
-      });
-
-      const gameRepository = new GameRepositoryImpl(database);
-      const repository = new GamePlayerRepositoryImpl(database);
-
-      // Act
-      await gameRepository.save(game);
-      await repository.save(player);
-      const instance = await repository.findBy(player.id);
-
-      // Assert
-      expect(instance?.id).toEqual(player.id);
-      expect(instance?.mode).toEqual(player.mode);
-      expect(instance?.user).toEqual(player.user);
-      expect(instance?.hand).toEqual(player.hand);
-    });
-
-    test("should not be able find a game if it did not save before", async () => {
-      // Arrange
-      const repository = new GamePlayerRepositoryImpl(database);
-
-      // Act
-      const instance = await repository.findBy(createGamePlayerId());
-
-      // Assert
-      expect(instance).toBeUndefined();
-    });
-
-    test("should be able to delete a player", async () => {
-      // Arrange
-      const game = createGame({
-        id: createGameId(),
-        name: "test",
-        players: [createGamePlayerId()],
-        cards: createSelectableCards([1, 2].map(createStoryPoint)),
-      });
-
-      const player = createGamePlayer({
-        id: game.players[0],
-        userId: createUserId(),
-        gameId: game.id,
-        cards: game.cards,
-      });
-
-      const gameRepository = new GameRepositoryImpl(database);
-      const repository = new GamePlayerRepositoryImpl(database);
-
-      // Act
-      await gameRepository.save(game);
-      await repository.save(player);
-      await repository.delete(player);
-      const instance = await repository.findBy(player.id);
-
-      // Assert
-      expect(instance).toBeUndefined();
-    });
+beforeAll(async () => {
+  testEnv = await initializeTestEnvironment({
+    projectId: "demo-project-1234",
+    database: {
+      host: "localhost",
+      port: 9000,
+    },
   });
+  database = testEnv.authenticatedContext("alice").database();
+});
+
+afterAll(async () => {
+  testEnv.cleanup();
+});
+
+afterEach(async () => {
+  await testEnv.clearDatabase();
+});
+
+test("should be able to save and find a game player", async () => {
+  // Arrange
+  const game = createGame({
+    id: createGameId(),
+    name: "test",
+    players: [createGamePlayerId()],
+    cards: createSelectableCards([1, 2].map(createStoryPoint)),
+  });
+
+  const player = createGamePlayer({
+    id: game.players[0],
+    userId: createUserId(),
+    gameId: game.id,
+    cards: game.cards,
+  });
+
+  const gameRepository = new GameRepositoryImpl(database);
+  const repository = new GamePlayerRepositoryImpl(database);
+
+  // Act
+  await gameRepository.save(game);
+  await repository.save(player);
+  const instance = await repository.findBy(player.id);
+
+  // Assert
+  expect(instance?.id).toEqual(player.id);
+  expect(instance?.mode).toEqual(player.mode);
+  expect(instance?.user).toEqual(player.user);
+  expect(instance?.hand).toEqual(player.hand);
+});
+
+test("should not be able find a game if it did not save before", async () => {
+  // Arrange
+  const repository = new GamePlayerRepositoryImpl(database);
+
+  // Act
+  const instance = await repository.findBy(createGamePlayerId());
+
+  // Assert
+  expect(instance).toBeUndefined();
+});
+
+test("should be able to delete a player", async () => {
+  // Arrange
+  const game = createGame({
+    id: createGameId(),
+    name: "test",
+    players: [createGamePlayerId()],
+    cards: createSelectableCards([1, 2].map(createStoryPoint)),
+  });
+
+  const player = createGamePlayer({
+    id: game.players[0],
+    userId: createUserId(),
+    gameId: game.id,
+    cards: game.cards,
+  });
+
+  const gameRepository = new GameRepositoryImpl(database);
+  const repository = new GamePlayerRepositoryImpl(database);
+
+  // Act
+  await gameRepository.save(game);
+  await repository.save(player);
+  await repository.delete(player);
+  const instance = await repository.findBy(player.id);
+
+  // Assert
+  expect(instance).toBeUndefined();
 });
