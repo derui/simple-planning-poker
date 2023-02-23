72d1ba3ea8f3398d45683b79dc23ff69a27ee394
derui <derutakayu@gmail.com>
2023-02-23 13:45:52 +0900

    feat: add base store

---
+++ src/status/store.ts
@@ -0,0 +1,42 @@
+import { Action, configureStore } from "@reduxjs/toolkit";
+import { combineEpics, createEpicMiddleware } from "redux-observable";
+import type { Dependencies } from "@/dependencies";
+import { DependencyRegistrar } from "@/utils/dependency-registrar";
+
+// INJECT REDUCER IMPORT HERE
+
+// INJECT EPIC IMPORT HERE
+
+const reducers = {
+  // do not format this structure.
+} as const;
+
+// eslint-disable-next-line
+export const createStore = (registrar: DependencyRegistrar<Dependencies>) => {
+  const rootEpics = [
+    // do not format this structure.
+  ];
+
+  // eslint-disable-next-line
+  const epicMiddleware = createEpicMiddleware<Action, Action, any>();
+
+  const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(epicMiddleware),
+  });
+
+  epicMiddleware.run(combineEpics<Action, Action, RootState>(...rootEpics));
+
+  return store;
+};
+
+/**
+ * create pure store. This store does not have any epic/thunk based process. To use for test.
+ */
+export const createPureStore = () => {
+  return configureStore({ reducer: reducers });
+};
+
+// export type of application state.
+export type RootState = ReturnType<ReturnType<typeof createStore>["getState"]>;
+export type AppDispatch = ReturnType<typeof createStore>["dispatch"];
