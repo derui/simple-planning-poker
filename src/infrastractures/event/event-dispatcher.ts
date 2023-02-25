0f18e574f33ac95b77a96c9a98393f23e79827e9
derui <derutakayu@gmail.com>
2023-02-25 16:08:00 +0900

    refactor: listeners

--- src/infrastractures/event/event-dispatcher.ts
+++ src/infrastractures/event/event-dispatcher.ts
@@ -1,11 +1,11 @@
-import { DefinedDomainEvents } from "@/domains/event";
+import { GenericDomainEvent } from "@/domains/event";
 import { EventDispatcher } from "@/usecases/base";
 import { DomainEventListener } from "./domain-event-listener";
 
 export class EventDispatcherImpl implements EventDispatcher {
   constructor(private listeners: DomainEventListener[]) {}
 
-  dispatch(event: DefinedDomainEvents): void {
+  dispatch(event: GenericDomainEvent): void {
     this.listeners.forEach((v) => v.handle(event));
   }
 }
