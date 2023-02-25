acd0cd68542943b51637515c764ee9e6cd9a8d2f
derui <derutakayu@gmail.com>
2023-02-25 10:54:17 +0900

    refactor: invitation domain

--- src/domains/invitation.test.ts
+++ src/domains/invitation.test.ts
@@ -1,13 +1,12 @@
 import { test, expect } from "vitest";
-import { createId } from "./game";
 import { create } from "./invitation";
 
 test("should create unique invitation of a game", () => {
   // Arrange
 
   // Act
-  const ret = create(createId("f"));
+  const ret = create("f");
 
   // Assert
-  expect(ret.signature).toEqual("252f10c83610ebca1a059c0bae8255eba2f95be4d1d7bcfa89d7248a82d9f111");
+  expect(ret).toBe("252f10c83610ebca1a059c0bae8255eba2f95be4d1d7bcfa89d7248a82d9f111");
 });
--- src/domains/invitation.ts
+++ src/domains/invitation.ts
@@ -1,19 +1,11 @@
-import { Id } from "./game";
 import sha256 from "crypto-js/sha256";
 import { Branded } from "./type";
 
-const tag = Symbol("InvitationSignature");
-export type InvitationSignature = Branded<string, typeof tag>;
+const _tag = Symbol("InvitationSignature");
+export type T = Branded<string, typeof _tag>;
 
-export interface T {
-  readonly gameId: Id;
-  readonly signature: InvitationSignature;
-}
+export const create = (base: string): T => {
+  const hash = sha256(base);
 
-export const create = (gameId: Id): T => {
-  const hash = sha256(gameId);
-  return {
-    gameId,
-    signature: hash.toString() as InvitationSignature,
-  };
+  return hash.toString() as T;
 };
