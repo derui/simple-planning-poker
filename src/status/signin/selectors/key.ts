b7a660532115719132566fb172cb2f535a8d1d44
derui <derutakayu@gmail.com>
2023-02-26 13:34:16 +0900

    refactor: delete old status

--- src/status/signin/selectors/key.ts
+++
@@ -1,8 +0,0 @@
-const SelectorKeys = {
-  authenticated: "signin:selector:authenticated",
-  authenticating: "signin:selector:authenticating",
-  joinedGamesState: "signin:selector:joinedGamesState",
-  currentUserState: "signin:selector:currentUserState",
-} as const;
-
-export default SelectorKeys;
--- src/status/signin/types.ts
+++
@@ -1,9 +0,0 @@
-import { Id } from "@/domains/user";
-
-export interface Authenticator {
-  signIn(email: string, password: string): Promise<Id>;
-
-  signUp(name: string, email: string, password: string): Promise<Id>;
-
-  currentUserIdIfExists(): Promise<Id | undefined>;
-}
