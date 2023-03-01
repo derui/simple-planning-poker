ca557c9ffcecd5e1ea533fc40789388316c9ae05
derui <derutakayu@gmail.com>
2023-03-01 20:01:42 +0900

    test: player card

--- src/components/presentations/player-card.tsx
+++ src/components/presentations/player-card.tsx
@@ -1,19 +1,68 @@
 import React, { useState } from "react";
 import classnames from "classnames";
+import { BaseProps, generateTestId } from "../base";
 import { UserMode } from "@/domains/game-player";
 
-interface PlayerCardComponentProps {
+interface Props extends BaseProps {
   mode: UserMode;
-  storyPoint: number | null;
+  display: string;
   selected: boolean;
-  showedDown: boolean;
+  opened: boolean;
 }
 
-const PlayerCardComponent: React.FunctionComponent<PlayerCardComponentProps> = (props) => {
+type State = "notSelected" | "handed" | "result";
+
+const styles = {
+  root: (state: State, transition: boolean) =>
+    classnames(
+      "flex",
+      "flex-col",
+      "h-24",
+      "w-12",
+      "rounded",
+      "bg-white",
+      "text-center",
+      "justify-center",
+      "border",
+      "border-primary-400",
+      "m-3",
+      "text-primary-500",
+      "transition-transform",
+      {
+        "bg-primary-400": state === "handed",
+        "text-secondary1-200": state === "handed",
+        "[transform:rotateY(180deg)]": state === "handed",
+      },
+      {
+        "bg-primary-400": state === "result",
+        "text-secondary1-200": state === "result",
+        "[transform:rotateY(180deg)]": state === "result",
+      },
+      {
+        "bg-white": transition,
+        "text-primary-500": transition,
+        "[transform:rotateY(0deg)]": transition,
+      }
+    ),
+
+  eye: classnames(
+    "block",
+    '[mask:url("/assets/svg/tabler-icons/eye.svg")]',
+    "[mask-size:contain]",
+    "bg-primary-400",
+    "h-10",
+    "w-10"
+  ),
+};
+
+// eslint-disable-next-line func-style
+export default function PlayerCardComponent(props: Props) {
+  const gen = generateTestId(props.testid);
   const [transition, setTransition] = useState(false);
+  const state = props.opened ? (props.selected ? "result" : "notSelected") : props.selected ? "handed" : "notSelected";
 
   React.useEffect(() => {
-    if (props.showedDown) {
+    if (props.opened) {
       const t = setTimeout(() => setTransition(true));
       return () => clearTimeout(t);
     }
@@ -21,26 +70,15 @@ const PlayerCardComponent: React.FunctionComponent<PlayerCardComponentProps> = (
 
   if (props.mode === UserMode.inspector) {
     return (
-      <span className="app__game__main__player-card--inspector">
-        <span className="app__game__main__player-card__eye"></span>
+      <span className={styles.root("notSelected", false)} data-testid={gen("root")} data-mode="inspector">
+        <span className={styles.eye} data-testid={gen("eye")}></span>
       </span>
     );
-  } else if (props.showedDown && props.storyPoint !== null) {
-    const className = classnames({
-      "app__game__main__player-card": true,
-      "app__game__main__player-card--result": true,
-      "app__game__main__player-card--transition": transition,
-    });
-    return <span className={className}>{props.storyPoint}</span>;
-  } else if (props.showedDown && props.storyPoint === null) {
-    return <span className="app__game__main__player-card">?</span>;
   }
 
-  const className = classnames({
-    "app__game__main__player-card": true,
-    "app__game__main__player-card--handed": props.selected,
-  });
-  return <span className={className}></span>;
-};
-
-export default PlayerCardComponent;
+  return (
+    <span className={styles.root(state, transition)} data-testid={gen("root")} data-mode="normal" data-state={state}>
+      {state === "result" ? props.display : ""}
+    </span>
+  );
+}
