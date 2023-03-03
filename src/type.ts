3db3ce064c4b64bfd87958bab20ee2187e0c93ff
derui <derutakayu@gmail.com>
2023-03-04 08:27:10 +0900

    refactor: loading status

--- src/type.ts
+++ src/type.ts
@@ -1,2 +1,58 @@
 // simple state of loading
-export type Loading = "loading" | "error" | "finished";
+
+const _loading = Symbol();
+const _error = Symbol();
+const _finished = Symbol();
+
+export type LoadingStatus = typeof _loading | typeof _error | typeof _finished;
+
+export type LoadingState = [undefined, typeof _loading];
+export type ErrorState<E> = [E, typeof _error];
+export type FinishedState<T> = [T, typeof _finished];
+
+/**
+ * simple loading handler
+ */
+export type Loadable<T, E = unknown> = LoadingState | ErrorState<E> | FinishedState<T>;
+
+/**
+ * check loading state is loading
+ */
+export const isLoading = function isLoading<T>(loadable: Loadable<T>): loadable is LoadingState {
+  return loadable[1] === _loading;
+};
+
+/**
+ * check loading state is error
+ */
+export const isError = function isError<E>(loadable: Loadable<unknown, E>): loadable is ErrorState<E> {
+  return loadable[1] === _error;
+};
+
+/**
+ * check loading state is finished
+ */
+export const isFinished = function isFinished<T>(loadable: Loadable<T>): loadable is FinishedState<T> {
+  return loadable[1] === _finished;
+};
+
+/**
+ * factory function for loading
+ */
+export const loading = function loading(): LoadingState {
+  return [undefined, _loading];
+};
+
+/**
+ * factory function for error;
+ */
+export const error = function error<E = never>(obj?: E extends never ? never : E | undefined): ErrorState<E> {
+  return [obj as E, _error];
+};
+
+/**
+ * factory function for finished
+ */
+export const finished = function finished<T>(obj: T): FinishedState<T> {
+  return [obj, _finished];
+};
