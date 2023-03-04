5c2de9310675cd8aef6a7401f53a427d1378bb02
derui <derutakayu@gmail.com>
2023-03-04 14:38:27 +0900

    refactor: point showcase

--- src/components/presentations/average-point-showcase.tsx
+++ src/components/presentations/average-point-showcase.tsx
@@ -1,33 +1,63 @@
-import React from "react";
+import classNames from "classnames";
+import { BaseProps, generateTestId } from "../base";
 
-interface Props {
+interface Props extends BaseProps {
   cardCounts: [number, number][];
   averagePoint: string;
 }
 
-const createResultDisplay = (index: number, storyPoint: number, count: number) => {
+const styles = {
+  root: classNames("flex", "justify-center", "px-3", "py-2", "relative", "h-32", "items-center"),
+  results: classNames("flex", "flex-none"),
+  average: classNames("flex", "flex-col", "justify-center", "items-center", "bg-primary-400", "rounded", "p-3"),
+  equal: classNames("flex-none", "text-lg", "text-secondary2-200"),
+  averageLabel: classNames("flex-none", "text-secondary2-200"),
+  averageValue: classNames("flex-none", "items-center", "text-xl"),
+
+  resultDisplay: {
+    root: classNames("flex", "flex-col", "items-center", "mr-3", "last-of-type:mr-6"),
+    card: classNames(
+      "flex",
+      "flex-col",
+      "h-16",
+      "w-12",
+      "rounded",
+      "border",
+      "border-primary-400",
+      "text-center",
+      "justify-center",
+      "m-3"
+    ),
+    count: classNames("text-black", "align-center"),
+  },
+};
+
+const createResultDisplay = (index: number, storyPoint: number, count: number, testid: string) => {
   return (
-    <div className="app__game__result-display" key={index}>
-      <span className="app__game__result-display__card">{storyPoint}</span>
-      <span className="app__game__result-display__count">{count} votes</span>
+    <div className={styles.resultDisplay.root} key={index} data-testid={testid}>
+      <span className={styles.resultDisplay.card}>{storyPoint}</span>
+      <span className={styles.resultDisplay.count}>{count} votes</span>
     </div>
   );
 };
 
-const AveragePointShowcaseComponent: React.FunctionComponent<Props> = (props) => {
+// eslint-disable-next-line func-style
+export function AveragePointShowcase(props: Props) {
+  const gen = generateTestId(props.testid);
+
   return (
-    <div className="app__game__average-point-showcase">
-      <div className="app__game__average-point-showcase__results">
-        {props.cardCounts.map(([storyPoint, count], index) => createResultDisplay(index, storyPoint, count))}
+    <div className={styles.root} data-testid={gen("root")}>
+      <div className={styles.results}>
+        {props.cardCounts.map(([storyPoint, count], index) =>
+          createResultDisplay(index, storyPoint, count, gen("resultCard"))
+        )}
       </div>
 
-      <div className="app__game__average-point-showcase__equal"> </div>
-      <div className="app__game__average-point-showcase__average">
-        <span className="app__game__average-point-showcase__average-label">Score</span>
-        <span className="app__game__average-point-showcase__average-value">{props.averagePoint}</span>
+      <div className={styles.equal}> </div>
+      <div className={styles.average} data-testid={gen("average")}>
+        <span className={styles.averageLabel}>Score</span>
+        <span className={styles.averageValue}>{props.averagePoint}</span>
       </div>
     </div>
   );
-};
-
-export default AveragePointShowcaseComponent;
+}
