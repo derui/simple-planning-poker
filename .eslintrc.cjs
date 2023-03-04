6df0deae74f3257c1850ca61a6f4a9e0cc819f59
derui <derutakayu@gmail.com>
2023-03-04 09:16:00 +0900

    feat: add story book

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 module.exports = {
   plugins: ["@typescript-eslint", "import", "unused-imports"],
   parser: '@typescript-eslint/parser',
-  extends:  [
-    "plugin:import/errors",
-    "plugin:import/warnings",
-  ],
+  extends: ["plugin:import/errors", "plugin:import/warnings"],
   env: {
     es6: true,
-    browser: true,
+    browser: true
   },
-  parserOptions:  {
-    ecmaVersion:  2020,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
-    ecmaFeatures:  {
-      jsx: true,  // Allows for the parsing of JSX
-    },
+  parserOptions: {
+    ecmaVersion: 2020,
+    // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',
+    // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
     // project: "./tsconfig.json",
   },
+
   rules: {
     "import/order": "error",
     "func-style": ["error", "expression"],
@@ -25,7 +25,7 @@ module.exports = {
     // "@typescript-eslint/no-unnecessary-type-assertion": "error",
     "unused-imports/no-unused-imports": "warn"
   },
-  settings:  {
+  settings: {
     "react": {
       "createClass": "createClass",
       "pragmaFrag": "Fragment"
@@ -33,6 +33,6 @@ module.exports = {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx", ".js"]
     },
-    "import/resolver": {},
-  },
-}
+    "import/resolver": {}
+  }
+};
