b97af07607014fd328cd9cdf5af1c9dcdeb26e24
derui <derutakayu@gmail.com>
2023-03-08 22:07:09 +0900

    refactor: round page

--- src/components/pages/round.tsx
+++ src/components/pages/round.tsx
@@ -1,113 +1,54 @@
 import * as React from "react";
-import { useNavigate, useParams } from "react-router";
+import classNames from "classnames";
 import { CardHolder } from "../presentations/card-holder";
-import { GameHeaderComponent } from "../presentations/game-header";
-import { EmptyCardHolderComponent } from "../presentations/empty-card-holder";
-import GameAreaComponent from "../presentations/game-area";
-import { UserMode } from "@/domains/game-player";
-import { Id } from "@/domains/game";
-import gameActionsContext, { GameActions } from "@/contexts/actions/game-actions";
-import {
-  useCurrentGameName,
-  useCurrentGameState,
-  useCurrentPlayerInformationState,
-  useCurrentPlayerSelectedCardState,
-  useSelectableCardsState,
-  useUserHandsState,
-} from "@/status/game/selectors";
-import userActionsContext from "@/contexts/actions/user-actions";
-import { mapFuture } from "@/status/util";
+import { GameHeaderContainer } from "../containers/game-header-container";
+import { GameAreaContainer } from "../containers/game-area-container";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { Skeleton } from "../presentations/skeleton";
+import { selectCards, selectPlayerHandedCard } from "@/status/selectors/game";
+import { isFinished } from "@/utils/loadable";
+import { handCard } from "@/status/actions/game";
 
-interface Props {}
-
-const createCardHolderComponent = ({ useSelectCard }: GameActions) => {
-  const selectCard = useSelectCard();
-  const cards = useSelectableCardsState().valueMaybe() ?? [];
-  const selectedIndex = useCurrentPlayerSelectedCardState()?.valueMaybe()?.index;
-
-  const props = {
-    displays: cards.map((v) => {
-      switch (v.kind) {
-        case "giveup":
-          return "?";
-        case "storypoint":
-          return v.storyPoint.value.toString();
-      }
-    }),
-    selectedIndex: selectedIndex ?? null,
-  };
-
-  return (
-    <CardHolder displays={props.displays} selectedIndex={props.selectedIndex} onSelect={(index) => selectCard(index)} />
-  );
+const styles = {
+  root: classNames("flex", "flex-col", "h-full"),
+  main: classNames("flex", "flex-auto", "p-2", "z-20"),
+  cardHolder: classNames("flex", "flex-auto", "p-2", "z-20"),
 };
 
-const GameContainer: React.FunctionComponent<Props> = () => {
-  const param = useParams<{ gameId: string }>();
-  const gameActions = React.useContext(gameActionsContext);
-  const component = createCardHolderComponent(gameActions);
-  const currentGameName = useCurrentGameName();
-  const userHands = useUserHandsState();
-  const changeName = React.useContext(userActionsContext).useChangeUserName();
-  const changeMode = gameActions.useChangeUserMode();
-  const currentUserInformation = useCurrentPlayerInformationState();
-  const currentUserName = currentUserInformation.name;
-  const currentUserMode = currentUserInformation.mode ?? UserMode.normal;
-  const currentGameState = useCurrentGameState();
-  const signature = currentGameState.valueMaybe()?.viewModel?.invitationSignature;
-  const currentStatus = mapFuture(currentGameState, (v) => v.status);
-  const openGame = gameActions.useOpenGame();
-  const leaveGame = gameActions.useLeaveGame();
-  const navigate = useNavigate();
-  const showDown = gameActions.useShowDown();
+// eslint-disable-next-line func-style
+function CardHolderContainer() {
+  const cards = useAppSelector(selectCards());
+  const playerHand = useAppSelector(selectPlayerHandedCard());
+  const dispatch = useAppDispatch();
 
-  React.useEffect(() => {
-    openGame(param.gameId as Id, () => {
-      navigate("/", { replace: true });
-    });
-  }, [param.gameId]);
-
-  React.useEffect(() => {
-    if (currentStatus.valueMaybe() === "ShowedDown") {
-      navigate(`/game/${param.gameId}/result`, { replace: true });
-    }
-  }, [currentStatus.valueMaybe()]);
-
-  let Component = <EmptyCardHolderComponent />;
-  if (currentUserMode === UserMode.normal) {
-    Component = component;
+  if (!isFinished(cards)) {
+    return (
+      <div className={styles.cardHolder}>
+        <Skeleton />
+      </div>
+    );
   }
 
-  const onShowDown = () => {
-    showDown();
-  };
+  const handleSelectCard = (index: number) => dispatch(handCard({ cardIndex: index }));
 
   return (
-    <div className="app__game">
-      <GameHeaderComponent
-        gameName={currentGameName}
-        userName={currentUserName || ""}
-        userMode={currentUserMode}
-        onChangeName={(name) => changeName(name)}
-        onChangeMode={(mode) => changeMode(mode)}
-        onLeaveGame={() => {
-          leaveGame();
-          navigate("/", { replace: true });
-        }}
-        origin={document.location.origin}
-        invitationSignature={signature || ""}
-      />
-      <main className="app__game__main">
-        <GameAreaComponent
-          onShowDown={onShowDown}
-          gameStatus={currentStatus}
-          lines={userHands}
-          userMode={currentUserMode}
-        />
+    <CardHolder
+      displays={cards[0].map((v) => v.display)}
+      selectedIndex={playerHand.cardIndex}
+      onSelect={handleSelectCard}
+    />
+  );
+}
+
+// eslint-disable-next-line func-style
+export function Round() {
+  return (
+    <div className={styles.root}>
+      <GameHeaderContainer />
+      <main className={styles.main}>
+        <GameAreaContainer />
       </main>
-      {Component}
+      <CardHolderContainer />
     </div>
   );
-};
-
-export default GameContainer;
+}
