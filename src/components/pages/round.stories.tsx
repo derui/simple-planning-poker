bcd3bfe93f65339970d9e0fb58c03eae089ce0aa
derui <derutakayu@gmail.com>
2023-03-09 22:59:23 +0900

    feat: add story for round

--- src/components/pages/round.stories.tsx
+++ src/components/pages/round.stories.tsx
@@ -6,6 +6,10 @@ import { Provider } from "react-redux";
 import { Round } from "./round";
 import twind from "@/twind.config.cjs";
 import { createPureStore } from "@/status/store";
+import { openGameSuccess } from "@/status/actions/game";
+import { randomGame, randomUser } from "@/test-lib";
+import { UserMode } from "@/domains/game-player";
+import { tryAuthenticateSuccess } from "@/status/actions/signin";
 
 install(twind);
 
@@ -21,6 +25,28 @@ type Story = StoryObj<typeof meta>;
 export const Loading: Story = {
   render() {
     const store = createPureStore();
+
+    return (
+      <Provider store={store}>
+        <Round />
+      </Provider>
+    );
+  },
+};
+
+export const Loaded: Story = {
+  render() {
+    const store = createPureStore();
+
+    const users = [randomUser({}), randomUser({})];
+    const owner = randomUser({});
+    store.dispatch(
+      openGameSuccess({
+        game: randomGame({ owner: owner.id, joinedPlayers: users.map((v) => ({ user: v.id, mode: UserMode.normal })) }),
+        players: users,
+      })
+    );
+    store.dispatch(tryAuthenticateSuccess({ user: owner }));
 
     return (
       <Provider store={store}>
