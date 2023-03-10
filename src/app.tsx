d996fbb1e228f1237c963a822dcd49ffe9d812c6
derui <derutakayu@gmail.com>
2023-03-11 08:20:48 +0900

    refactor: apply all new structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,91 +1,79 @@
-import React, { Suspense } from "react";
+import React, { PropsWithChildren } from "react";
 import { useLocation } from "react-router";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { RecoilRoot } from "recoil";
-import TransitionFallback from "./components/presentations/transition-fallback";
-import { useAuthenticatedState } from "./status/signin/selectors";
+import { useAppSelector } from "./components/hooks";
+import { selectAuthenticating } from "./status/selectors/auth";
 import lazyImport from "./utils/lazy-import";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
-  const state = useAuthenticatedState();
+// eslint-disable-next-line func-style
+function PrivateRoute({ children }: PropsWithChildren) {
+  const state = useAppSelector(selectAuthenticating());
   const location = useLocation();
 
   if (!state) {
     return <Navigate replace to="/signin" state={{ from: location }} />;
   }
 
-  return children;
-};
+  return <>{children}</>;
+}
 
 export const App: React.FunctionComponent<{}> = () => {
-  const LaziedGameContainer = React.lazy(() => lazyImport(import("./components/containers/game-container")));
-  const LaziedGameResultContainer = React.lazy(() =>
-    lazyImport(import("./components/containers/game-result-container"))
-  );
-  const LaziedGameCreatorContainer = React.lazy(() =>
-    lazyImport(import("./components/containers/game-creator-container"))
-  );
-  const LaziedGameSelectorContainer = React.lazy(() =>
-    lazyImport(import("./components/containers/game-selector-container"))
-  );
-  const LaziedInvitationContainer = React.lazy(() =>
-    lazyImport(import("./components/containers/invitation-container"))
-  );
-  const LaziedSignInContainer = React.lazy(() => lazyImport(import("./components/containers/signin-container")));
-  const LaziedSignUpContainer = React.lazy(() => lazyImport(import("./components/containers/signup-container")));
+  const LaziedGameContainer = React.lazy(() => lazyImport(import("./components/pages/round")));
+  const LaziedGameResultContainer = React.lazy(() => lazyImport(import("./components/pages/round-result")));
+  const LaziedGameCreatorContainer = React.lazy(() => lazyImport(import("./components/pages/create-game")));
+  const LaziedGameSelectorContainer = React.lazy(() => lazyImport(import("./components/pages/select-game")));
+  const LaziedInvitationContainer = React.lazy(() => lazyImport(import("./components/pages/join-game")));
+  const LaziedSignInContainer = React.lazy(() => lazyImport(import("./components/pages/signin")));
+  const LaziedSignUpContainer = React.lazy(() => lazyImport(import("./components/pages/signup")));
 
   return (
-    <RecoilRoot>
-      <Suspense fallback={<TransitionFallback />}>
-        <div className="app__root">
-          <BrowserRouter>
-            <Routes>
-              <Route
-                path="/game/create"
-                element={
-                  <PrivateRoute>
-                    <LaziedGameCreatorContainer />
-                  </PrivateRoute>
-                }
-              ></Route>
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <LaziedGameSelectorContainer />
-                  </PrivateRoute>
-                }
-              ></Route>
-              <Route
-                path="/game/:gameId"
-                element={
-                  <PrivateRoute>
-                    <LaziedGameContainer />
-                  </PrivateRoute>
-                }
-              ></Route>
-              <Route
-                path="/game/:gameId/result"
-                element={
-                  <PrivateRoute>
-                    <LaziedGameResultContainer />
-                  </PrivateRoute>
-                }
-              ></Route>
-              <Route
-                path="/invitation/:signature"
-                element={
-                  <PrivateRoute>
-                    <LaziedInvitationContainer />
-                  </PrivateRoute>
-                }
-              ></Route>
-              <Route path="/signin" element={<LaziedSignInContainer />}></Route>
-              <Route path="/signup" element={<LaziedSignUpContainer />}></Route>
-            </Routes>
-          </BrowserRouter>
-        </div>
-      </Suspense>
-    </RecoilRoot>
+    <div className="app__root">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/game/create"
+            element={
+              <PrivateRoute>
+                <LaziedGameCreatorContainer />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <LaziedGameSelectorContainer />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/game/:gameId"
+            element={
+              <PrivateRoute>
+                <LaziedGameContainer />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/game/:gameId/result"
+            element={
+              <PrivateRoute>
+                <LaziedGameResultContainer />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route
+            path="/invitation/:signature"
+            element={
+              <PrivateRoute>
+                <LaziedInvitationContainer />
+              </PrivateRoute>
+            }
+          ></Route>
+          <Route path="/signin" element={<LaziedSignInContainer />}></Route>
+          <Route path="/signup" element={<LaziedSignUpContainer />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -3,51 +3,28 @@ import { connectDatabaseEmulator, getDatabase } from "firebase/database";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import React from "react";
 import * as ReactDOM from "react-dom";
+import { install } from "@twind/core";
 import { App } from "./app";
 import { firebaseConfig } from "./firebase.config";
 import { FirebaseAuthenticator } from "./infrastractures/authenticator";
 import { EventDispatcherImpl } from "./infrastractures/event/event-dispatcher";
-import { GameCreatedEventListener } from "./infrastractures/event/game-created-event-listener";
 import { GameRepositoryImpl } from "./infrastractures/game-repository";
-import { GameShowedDownEventListener } from "./infrastractures/event/game-showed-down-event-listener";
 import { HandCardUseCase } from "./usecases/hand-card";
 import { ShowDownUseCase } from "./usecases/show-down";
-import { NewGameUseCase } from "./usecases/new-game";
 import { gameObserverContext } from "./contexts/observer";
 import { GameObserverImpl } from "./infrastractures/game-observer";
 import { UserRepositoryImpl } from "./infrastractures/user-repository";
 import { JoinUserUseCase } from "./usecases/join-user";
-import { NewGameStartedEventListener } from "./infrastractures/event/new-game-started-event-listener";
 import { ChangeUserNameUseCase } from "./usecases/change-user-name";
 import { ChangeUserModeUseCase } from "./usecases/change-user-mode";
-import { GamePlayerRepositoryImpl } from "./infrastractures/game-player-repository";
-import * as JoinService from "./domains/join-service";
-import { UserLeaveFromGameEventListener } from "./infrastractures/event/user-leave-from-game-event-listener";
 import { createDependencyRegistrar } from "./utils/dependency-registrar";
 import { ApplicationDependencyRegistrar } from "./dependencies";
 import { UserObserverImpl } from "./infrastractures/user-observer";
-import signInActionContext, { SigninActions } from "./contexts/actions/signin-actions";
-import createUseApplyAuthenticated from "./status/signin/actions/use-apply-authenticated";
-import createUseSignIn from "./status/signin/actions/use-signin";
-import createUseSignUp from "./status/signin/actions/use-signup";
-import userActionsContext, { UserActions } from "./contexts/actions/user-actions";
-import createUseChangeUserName from "./status/user/actions/use-change-user-name";
-import createUseCreateGame from "./status/game/actions/use-create-game";
-import gameActionsContext, { GameActions } from "./contexts/actions/game-actions";
-import createUseOpenGame from "./status/game/actions/use-open-game";
-import createUseJoinUser from "./status/game/actions/use-join-user";
-import createUseLeaveGame from "./status/game/actions/use-leave-game";
-import createUseNewGame from "./status/game/actions/use-new-game";
-import createUseSelectCard from "./status/game/actions/use-select-card";
-import createUseSelectGame from "./status/game/actions/use-select-game";
-import createUseShowDown from "./status/game/actions/use-show-down";
 import { CreateGameUseCase } from "./usecases/create-game";
 import { LeaveGameUseCase } from "./usecases/leave-game";
-import createUseChangeUserMode from "./status/game/actions/use-change-user-mode";
-import { initializeGameQuery } from "./status/game/atoms/game-query";
-import { initializeUserQuery } from "./status/user/atoms/user-state-query";
 import config from "./twind.config.cjs";
-import { install } from "@twind/core";
+import { RoundRepositoryImpl } from "./infrastractures/round-repository";
+import { NewRoundUseCase } from "./usecases/new-round";
 
 const firebaseApp = initializeApp(firebaseConfig);
 
@@ -58,79 +35,34 @@ if (location.hostname === "localhost") {
   connectAuthEmulator(auth, "http://localhost:9099");
 }
 
-const gameRepository = new GameRepositoryImpl(database);
+const gameRepository = new GameRepositoryImpl(database, new RoundRepositoryImpl(database));
 const userRepository = new UserRepositoryImpl(database);
-const gamePlayerRepository = new GamePlayerRepositoryImpl(database);
 
-const dispatcher = new EventDispatcherImpl([
-  new GameCreatedEventListener(gamePlayerRepository),
-  new GameShowedDownEventListener(database),
-  new NewGameStartedEventListener(database),
-  new UserLeaveFromGameEventListener(gamePlayerRepository),
-]);
+const dispatcher = new EventDispatcherImpl([]);
 
 const registrar = createDependencyRegistrar() as ApplicationDependencyRegistrar;
 registrar.register("userRepository", userRepository);
 registrar.register("userObserver", new UserObserverImpl(database, registrar.resolve("userRepository")));
-registrar.register("gamePlayerRepository", gamePlayerRepository);
 registrar.register("gameRepository", gameRepository);
-registrar.register("handCardUseCase", new HandCardUseCase(dispatcher, registrar.resolve("gamePlayerRepository")));
+registrar.register("handCardUseCase", new HandCardUseCase(registrar.resolve("gameRepository")));
 registrar.register("showDownUseCase", new ShowDownUseCase(dispatcher, registrar.resolve("gameRepository")));
-registrar.register("newGameUseCase", new NewGameUseCase(dispatcher, registrar.resolve("gameRepository")));
+registrar.register("newRoundUseCase", new NewRoundUseCase(dispatcher, registrar.resolve("gameRepository")));
 registrar.register("createGameUseCase", new CreateGameUseCase(dispatcher, registrar.resolve("gameRepository")));
-registrar.register("leaveGameUseCase", new LeaveGameUseCase(dispatcher, registrar.resolve("userRepository")));
-registrar.register(
-  "changeUserModeUseCase",
-  new ChangeUserModeUseCase(dispatcher, registrar.resolve("gamePlayerRepository"))
-);
+registrar.register("leaveGameUseCase", new LeaveGameUseCase(registrar.resolve("gameRepository")));
+registrar.register("changeUserModeUseCase", new ChangeUserModeUseCase(registrar.resolve("gameRepository")));
 registrar.register(
   "joinUserUseCase",
-  new JoinUserUseCase(
-    dispatcher,
-    registrar.resolve("userRepository"),
-    JoinService.create(registrar.resolve("gameRepository"), registrar.resolve("gamePlayerRepository"))
-  )
+  new JoinUserUseCase(dispatcher, registrar.resolve("userRepository"), registrar.resolve("gameRepository"))
 );
 registrar.register("authenticator", new FirebaseAuthenticator(auth, database, registrar.resolve("userRepository")));
 registrar.register("changeUserNameUseCase", new ChangeUserNameUseCase(dispatcher, registrar.resolve("userRepository")));
 registrar.register("gameObserver", new GameObserverImpl(database, registrar.resolve("gameRepository")));
 
-// initialize atoms before launch
-initializeGameQuery(registrar);
-initializeUserQuery(registrar);
-
-const gameAction: GameActions = {
-  useCreateGame: createUseCreateGame(registrar),
-  useOpenGame: createUseOpenGame(registrar),
-  useChangeUserMode: createUseChangeUserMode(registrar),
-  useJoinUser: createUseJoinUser(registrar),
-  useLeaveGame: createUseLeaveGame(registrar),
-  useNewGame: createUseNewGame(registrar),
-  useSelectCard: createUseSelectCard(registrar),
-  useSelectGame: createUseSelectGame(),
-  useShowDown: createUseShowDown(registrar),
-};
-const signInActions: SigninActions = {
-  useApplyAuthenticated: createUseApplyAuthenticated(registrar),
-  useSignIn: createUseSignIn(registrar),
-  useSignUp: createUseSignUp(registrar),
-};
-const userActions: UserActions = {
-  useChangeUserName: createUseChangeUserName(registrar),
-};
-
 install(config);
 
 ReactDOM.render(
-  <signInActionContext.Provider value={signInActions}>
-    <gameActionsContext.Provider value={gameAction}>
-      <userActionsContext.Provider value={userActions}>
-        <gameObserverContext.Provider value={new GameObserverImpl(database, gameRepository)}>
-          <App />
-        </gameObserverContext.Provider>
-      </userActionsContext.Provider>
-    </gameActionsContext.Provider>
-  </signInActionContext.Provider>,
-
+  <gameObserverContext.Provider value={new GameObserverImpl(database, gameRepository)}>
+    <App />
+  </gameObserverContext.Provider>,
   document.getElementById("root")
 );
