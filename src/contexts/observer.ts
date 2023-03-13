4b9cead6c8df33b0b8b8512803c026018df2a17a
derui <derutakayu@gmail.com>
2023-03-13 20:58:06 +0900

    feat: add round observer

--- src/contexts/observer.ts
+++ src/contexts/observer.ts
@@ -1,9 +1,16 @@
 import { createContext } from "react";
 import * as Game from "@/domains/game";
 import * as User from "@/domains/user";
+import * as Round from "@/domains/round";
 
 export interface GameObserver {
   subscribe(gameId: Game.Id, subscriber: (game: Game.T) => void): void;
+
+  unsubscribe(): void;
+}
+
+export interface RoundObserver {
+  subscribe(gameId: Round.Id, subscriber: (round: Round.T) => void): void;
 
   unsubscribe(): void;
 }
---
+++ src/infrastractures/round-observer.ts
@@ -0,0 +1,32 @@
+import { Database, ref, onValue, Unsubscribe, DataSnapshot } from "firebase/database";
+import { deserializeFrom } from "./round-database-deserializer";
+import { RoundObserver } from "@/contexts/observer";
+import { T, Id } from "@/domains/round";
+
+export class RoundObserverImpl implements RoundObserver {
+  private _unsubscriber: Unsubscribe | null = null;
+  private _subscribingRoundId: Id | null = null;
+  constructor(private database: Database) {}
+
+  subscribe(roundId: Id, subscriber: (round: T) => void): void {
+    this.unsubscribe();
+
+    this._subscribingRoundId = roundId;
+    const _subscriber = (snapshot: DataSnapshot) => {
+      const round = deserializeFrom(roundId, snapshot);
+      if (!round) {
+        return;
+      }
+
+      subscriber(round);
+    };
+
+    this._unsubscriber = onValue(ref(this.database, `rounds/${roundId}`), _subscriber);
+  }
+
+  unsubscribe(): void {
+    if (this._unsubscriber && this._subscribingRoundId) {
+      this._unsubscriber();
+    }
+  }
+}
