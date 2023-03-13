4ade45836d1aa691b23e18dd9d2302d830f9165f
derui <derutakayu@gmail.com>
2023-03-13 20:47:18 +0900

    refactor: normalize round data into state

--- src/status/slices/round.test.ts
+++ src/status/slices/round.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import { openGameSuccess } from "../actions/game";
+import { newRoundSuccess, openGameSuccess } from "../actions/game";
 import { getInitialState, reducer } from "./round";
 import { randomGame } from "@/test-lib";
 import * as SelectableCards from "@/domains/selectable-cards";
@@ -15,6 +15,27 @@ test("get round as open game", () => {
   const game = randomGame({ cards });
 
   const ret = reducer(getInitialState(), openGameSuccess({ game, players: [] }));
+
+  expect(ret.instance).toEqual({
+    id: game.round.id,
+    hands: {},
+    cards: {
+      [0]: { card: cards[0], order: 0 },
+      [1]: { card: cards[1], order: 1 },
+    },
+    count: 1,
+    joinedPlayers: {
+      [game.owner]: UserMode.normal,
+    },
+    finished: false,
+  });
+});
+
+test("get round after new round", () => {
+  const cards = SelectableCards.create([1, 2].map(StoryPoint.create));
+  const game = randomGame({ cards });
+
+  const ret = reducer(getInitialState(), newRoundSuccess(game));
 
   expect(ret.instance).toEqual({
     id: game.round.id,
--- src/status/slices/round.ts
+++ src/status/slices/round.ts
@@ -1,10 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { openGameSuccess } from "../actions/game";
-import { handCardSuccess } from "../actions/round";
+import { WritableDraft } from "immer/dist/internal";
+import { newRoundSuccess, openGameSuccess } from "../actions/game";
+import * as RoundAction from "../actions/round";
 import * as Round from "@/domains/round";
 import * as Card from "@/domains/card";
 import * as User from "@/domains/user";
 import * as UserHand from "@/domains/user-hand";
+import * as SelectableCards from "@/domains/selectable-cards";
 import { UserMode } from "@/domains/game-player";
 
 interface RoundState {
@@ -21,34 +23,88 @@ interface RoundState {
 
 const initialState = {} as RoundState satisfies RoundState;
 
+const normalize = function normalize(draft: WritableDraft<RoundState>, round: Round.T) {
+  let cards: SelectableCards.T | undefined = undefined;
+  if (Round.isRound(round)) {
+    cards = round.selectableCards;
+  }
+
+  if (!draft.instance) {
+    draft.instance = {
+      id: round.id,
+      cards:
+        cards?.reduce<Record<number, { card: Card.T; order: number }>>((accum, card, index) => {
+          accum[index] = { card, order: index };
+          return accum;
+        }, {}) ?? {},
+      count: round.count,
+      hands: round.hands,
+      joinedPlayers: round.joinedPlayers.reduce<Record<User.Id, UserMode>>((accum, obj) => {
+        accum[obj.user] = obj.mode;
+        return accum;
+      }, {}),
+      finished: Round.isFinishedRound(round),
+    };
+  } else {
+    draft.instance.id = round.id;
+    draft.instance.cards =
+      cards?.reduce<Record<number, { card: Card.T; order: number }>>((accum, card, index) => {
+        accum[index] = { card, order: index };
+        return accum;
+      }, {}) ?? {};
+    draft.instance.count = round.count;
+    draft.instance.hands = round.hands;
+    draft.instance.joinedPlayers = round.joinedPlayers.reduce<Record<User.Id, UserMode>>((accum, obj) => {
+      accum[obj.user] = obj.mode;
+      return accum;
+    }, {});
+    draft.instance.finished = Round.isFinishedRound(round);
+  }
+};
+
 const slice = createSlice({
   name: "round",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(openGameSuccess, (draft, { payload }) => {
-      draft.instance = {
-        id: payload.game.round.id,
-        cards: payload.game.cards.reduce<Record<number, { card: Card.T; order: number }>>((accum, card, index) => {
-          accum[index] = { card, order: index };
-          return accum;
-        }, {}),
-        count: payload.game.round.count,
-        hands: payload.game.round.hands,
-        joinedPlayers: payload.game.joinedPlayers.reduce<Record<User.Id, UserMode>>((accum, obj) => {
-          accum[obj.user] = obj.mode;
-          return accum;
-        }, {}),
-        finished: Round.isFinishedRound(payload.game.round),
-      };
+      normalize(draft, payload.game.round);
     });
 
-    builder.addCase(handCardSuccess, (draft, { payload }) => {
+    builder.addCase(RoundAction.handCardSuccess, (draft, { payload }) => {
       if (!draft.instance) {
         return;
       }
 
-      draft.instance.hands = payload.hands;
+      normalize(draft, payload);
+    });
+
+    builder.addCase(RoundAction.giveUpSuccess, (draft, { payload }) => {
+      if (!draft.instance) {
+        return;
+      }
+
+      normalize(draft, payload);
+    });
+
+    builder.addCase(RoundAction.changeUserModeSuccess, (draft, { payload }) => {
+      if (!draft.instance) {
+        return;
+      }
+
+      normalize(draft, payload);
+    });
+
+    builder.addCase(RoundAction.showDownSuccess, (draft, { payload }) => {
+      if (!draft.instance) {
+        return;
+      }
+
+      normalize(draft, payload);
+    });
+
+    builder.addCase(newRoundSuccess, (draft, { payload }) => {
+      normalize(draft, payload.round);
     });
   },
 });
