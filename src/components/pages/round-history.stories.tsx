5d4644b5989a0b03efb9a684710a83cd8e2091f4
derui <deruakayu@gmail.com>
2023-05-02 15:05:23 +0900

    fix: story display

--- src/components/pages/round-history.stories.tsx
+++ src/components/pages/round-history.stories.tsx
@@ -8,12 +8,9 @@ import RoundHistoryPage from "./round-history";
 import twind from "@/twind.config.cjs";
 import { createPureStore } from "@/status/store";
 import { openGameSuccess } from "@/status/actions/game";
-import { randomGame, randomRound, randomUser } from "@/test-lib";
-import * as Game from "@/domains/game";
+import { randomFinishedRound, randomGame, randomUser } from "@/test-lib";
 import { tryAuthenticateSuccess } from "@/status/actions/signin";
-import { giveUp, estimated } from "@/domains/user-estimation";
-import { notifyRoundUpdated, showDownSuccess } from "@/status/actions/round";
-import { Round, showDown, takePlayerEstimation } from "@/domains/round";
+import { openFinishedRoundsSuccess, openRoundHistory } from "@/status/actions/round";
 
 install(twind);
 
@@ -54,23 +51,17 @@ export const Loaded: Story = {
 
     const users = [randomUser({}), randomUser({})];
     const owner = randomUser({});
-    let game = randomGame({ owner: owner.id });
-    let round = randomRound({ id: game.round, cards: game.cards });
-    users.forEach((user) => {
-      game = Game.joinUserAsPlayer(game, user.id, Game.makeInvitation(game))[0];
-    });
-
+    const game = randomGame({ owner: owner.id });
     store.dispatch(
       openGameSuccess({
         game,
         players: users,
       })
     );
-    store.dispatch(notifyRoundUpdated(round));
     store.dispatch(tryAuthenticateSuccess({ user: owner }));
-    round = takePlayerEstimation(round, owner.id, giveUp());
-    round = takePlayerEstimation(round, users[0].id, estimated(game.cards[0]));
-    store.dispatch(showDownSuccess(showDown(round as Round, new Date())[0]));
+    const round = randomFinishedRound();
+    store.dispatch(openFinishedRoundsSuccess([round]));
+    store.dispatch(openRoundHistory(round.id));
 
     const route = createMemoryRouter(
       [
