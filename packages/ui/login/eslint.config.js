b4199d7edc457364e59adb2c209e3ef8e0193a9f
derui <derutakayu@gmail.com>
2024-08-28 22:22:26 +0900

    chore: use pre-compied package pattern

--- packages/ui/login/eslint.config.js
+++
@@ -1,3 +0,0 @@
-import defaultConfig from "@spp/eslint-config";
-
-export default [...defaultConfig];
--- packages/ui/login/src/index.ts
+++
@@ -1,1 +0,0 @@
-// Export only shared `logic`. Please DO NOT EXPORT internal state or internal dependency
