aade7ed3484d601bc938f67da16ac09ec6fd2cbc
derui <derutakayu@gmail.com>
2024-09-11 21:22:12 +0900

    feat: add memory version authenticator

---
+++ features/authenticator/src/memory.ts
@@ -0,0 +1,40 @@
+import { User, UserRepository } from "@spp/shared-domain";
+import { Authenticator } from "./type.js";
+
+/**
+ * In memory version authenticator
+ */
+export class MemoryAuthenticator implements Authenticator {
+  constructor(private userRepository: UserRepository.T) {}
+  async signIn(email: string, password: string): Promise<User.Id> {
+    try {
+      const userId = User.createId(`${email}/${password}`);
+      const user = await this.userRepository.findBy(userId);
+      if (!user) {
+        throw Error("Not found user");
+      }
+
+      return user.id;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  async signUp(name: string, email: string, password: string): Promise<User.Id> {
+    try {
+      const userId = User.createId(`${email}/${password}`);
+      const user = User.create({ id: userId, name });
+      this.userRepository.save(user);
+
+      return userId;
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  async currentUserIdIfExists(): Promise<User.Id | undefined> {
+    return Promise.resolve(undefined);
+  }
+}
