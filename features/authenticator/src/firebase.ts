f818ae88ed9ef8eefbb72ae6e90c04a32a27fe71
derui <derutakayu@gmail.com>
2024-09-15 14:54:54 +0900

    style: use function style

--- features/authenticator/src/firebase.ts
+++ features/authenticator/src/firebase.ts
@@ -2,55 +2,59 @@ import { Auth, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEma
 import { User, UserRepository } from "@spp/shared-domain";
 import { Authenticator } from "./type.js";
 
-export class FirebaseAuthenticator implements Authenticator {
-  constructor(
-    private auth: Auth,
-    private userRepository: UserRepository.T
-  ) {}
-  async signIn(email: string, password: string): Promise<User.Id> {
-    try {
-      const auth = await signInWithEmailAndPassword(this.auth, email, password);
-      const uid = auth.user.uid;
+/**
+ * Get new instance of authenticator that is combined with Firebase.Auth
+ */
+export const newFirebaseAuthenticator = function newFirebaseAuthenticator(
+  auth: Auth,
+  userRepository: UserRepository.T
+): Authenticator {
+  return {
+    async signIn(email: string, password: string): Promise<User.Id> {
+      try {
+        const authenticated = await signInWithEmailAndPassword(auth, email, password);
+        const uid = authenticated.user.uid;
 
-      const userId = User.createId(uid);
-      const user = await this.userRepository.findBy(userId);
-      if (!user) {
-        throw Error("Not found user");
-      }
-
-      return user.id;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  }
-
-  async signUp(name: string, email: string, password: string): Promise<User.Id> {
-    try {
-      const auth = await createUserWithEmailAndPassword(this.auth, email, password);
-      const uid = auth.user.uid;
-
-      const userId = User.createId(uid);
-      const user = User.create({ id: userId, name });
-      await this.userRepository.save(user);
-
-      return userId;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  }
-
-  async currentUserIdIfExists(): Promise<User.Id | undefined> {
-    return new Promise((resolve) => {
-      onAuthStateChanged(this.auth, (user) => {
-        if (user) {
-          const uid = user.uid;
-          resolve(User.createId(uid));
-        } else {
-          resolve(undefined);
+        const userId = User.createId(uid);
+        const user = await userRepository.findBy(userId);
+        if (!user) {
+          throw Error("Not found user");
         }
+
+        return user.id;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+
+    async signUp(name: string, email: string, password: string): Promise<User.Id> {
+      try {
+        const authenticated = await createUserWithEmailAndPassword(auth, email, password);
+        const uid = authenticated.user.uid;
+
+        const userId = User.createId(uid);
+        const user = User.create({ id: userId, name });
+        await userRepository.save(user);
+
+        return userId;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
+
+    async currentUserIdIfExists(): Promise<User.Id | undefined> {
+      return new Promise((resolve) => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            const uid = user.uid;
+            resolve(User.createId(uid));
+          } else {
+            resolve(undefined);
+          }
+        });
       });
-    });
-  }
-}
+    },
+  };
+};
--- features/authenticator/src/memory.ts
+++ features/authenticator/src/memory.ts
@@ -4,37 +4,38 @@ import { Authenticator } from "./type.js";
 /**
  * In memory version authenticator
  */
-export class MemoryAuthenticator implements Authenticator {
-  constructor(private userRepository: UserRepository.T) {}
-  async signIn(email: string, password: string): Promise<User.Id> {
-    try {
-      const userId = User.createId(`${email}/${password}`);
-      const user = await this.userRepository.findBy(userId);
-      if (!user) {
-        throw Error("Not found user");
+export const newMemoryAuthenticator = function newMemoryAuthenticator(userRepository: UserRepository.T): Authenticator {
+  return {
+    async signIn(email: string, password: string): Promise<User.Id> {
+      try {
+        const userId = User.createId(`${email}/${password}`);
+        const user = await userRepository.findBy(userId);
+        if (!user) {
+          throw Error("Not found user");
+        }
+
+        return user.id;
+      } catch (e) {
+        console.error(e);
+        throw e;
       }
+    },
 
-      return user.id;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  }
+    async signUp(name: string, email: string, password: string): Promise<User.Id> {
+      try {
+        const userId = User.createId(`${email}/${password}`);
+        const user = User.create({ id: userId, name });
+        await userRepository.save(user);
 
-  async signUp(name: string, email: string, password: string): Promise<User.Id> {
-    try {
-      const userId = User.createId(`${email}/${password}`);
-      const user = User.create({ id: userId, name });
-      await this.userRepository.save(user);
+        return userId;
+      } catch (e) {
+        console.error(e);
+        throw e;
+      }
+    },
 
-      return userId;
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-  }
-
-  async currentUserIdIfExists(): Promise<User.Id | undefined> {
-    return Promise.resolve(undefined);
-  }
-}
+    async currentUserIdIfExists(): Promise<User.Id | undefined> {
+      return Promise.resolve(undefined);
+    },
+  };
+};
