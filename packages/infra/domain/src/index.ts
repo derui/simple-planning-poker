1d230a1445c581d74c0775622acc0b15f2427b6b
derui <derutakayu@gmail.com>
2024-09-22 09:10:10 +0900

    fix: add public interface for domain infra

--- packages/infra/domain/src/index.ts
+++ packages/infra/domain/src/index.ts
@@ -1,1 +1,8 @@
 // Add public interface
+export { newEventDispatcher } from "./event/event-dispatcher.js";
+export * from "./game-observer.js";
+export * from "./game-repository.js";
+export * from "./user-observer.js";
+export * from "./user-repository.js";
+export * from "./voting-observer.js";
+export * from "./voting-repository.js";
