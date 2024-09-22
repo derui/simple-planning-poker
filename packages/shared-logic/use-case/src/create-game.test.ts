4092ec9ae3119a621a5c90d807a80a6d369334df
derui <derutakayu@gmail.com>
2024-09-22 14:28:32 +0900

    fix: should not throw exception from use case

--- packages/shared-logic/use-case/src/create-game.test.ts
+++ packages/shared-logic/use-case/src/create-game.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "vitest";
 import * as sinon from "sinon";
 import { newCreateGameUseCase } from "./create-game.js";
-import { ApplicablePoints, DomainEvent, StoryPoint, User, Game } from "@spp/shared-domain";
+import { ApplicablePoints, DomainEvent, StoryPoint, User, Game, GameRepository } from "@spp/shared-domain";
 import { newMemoryGameRepository } from "@spp/shared-domain/mock/game-repository";
 
 test("should return error if numbers is invalid", async () => {
@@ -91,4 +91,24 @@ test("should dispatch game created event", async () => {
   } else {
     expect.fail("should be GameCreated");
   }
+});
+test("should fail if repository throws error", async () => {
+  // Arrange
+  const input = {
+    name: "foo",
+    points: [1],
+    createdBy: User.createId(),
+  };
+  const dispatcher = sinon.fake();
+  const repository: GameRepository.T = {
+    ...newMemoryGameRepository(),
+    save: sinon.fake.throws("error"),
+  };
+  const useCase = newCreateGameUseCase(dispatcher, repository);
+
+  // Act
+  const ret = await useCase(input);
+
+  // Assert
+  expect(ret.kind).toEqual("failed");
 });
--- packages/shared-logic/use-case/src/create-game.ts
+++ packages/shared-logic/use-case/src/create-game.ts
@@ -10,7 +10,8 @@ export interface CreateGameUseCaseInput {
 export type CreateGameUseCaseOutput =
   | { kind: "success"; game: Game.T }
   | { kind: "invalidStoryPoint" }
-  | { kind: "invalidStoryPoints" };
+  | { kind: "invalidStoryPoints" }
+  | { kind: "failed" };
 
 export const newCreateGameUseCase = function newCreateGameUseCase(
   dispatcher: EventDispatcher,
@@ -39,7 +40,13 @@ export const newCreateGameUseCase = function newCreateGameUseCase(
       voting: votingId,
     });
 
-    await gameRepository.save(game);
+    try {
+      await gameRepository.save(game);
+    } catch (e) {
+      console.warn(e);
+      return { kind: "failed" };
+    }
+
     dispatcher(event);
 
     return { kind: "success", game };
