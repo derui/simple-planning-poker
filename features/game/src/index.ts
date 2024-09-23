ed02205e289dc6c2f967882eccb9748994b69d9d
derui <derutakayu@gmail.com>
2024-09-23 11:45:47 +0900

    feat: export public interface

--- features/game/src/index.ts
+++ features/game/src/index.ts
@@ -1,1 +1,5 @@
 // Add public interface
+export * from "./hooks/facade.js";
+export * from "./atoms/game.js";
+export * from "./components/game-creator.js";
+export * from "./components/game-index.js";
