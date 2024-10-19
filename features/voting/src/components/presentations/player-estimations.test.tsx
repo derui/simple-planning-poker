ad730edb11b0a329aeaa182c656cecd7cb114084
derui <derutakayu@gmail.com>
2024-10-19 17:04:09 +0900

    feat: add reveal button into estimation

--- features/voting/src/components/presentations/player-estimations.test.tsx
+++ features/voting/src/components/presentations/player-estimations.test.tsx
@@ -1,6 +1,7 @@
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import { test, expect, afterEach } from "vitest";
 import { PlayerEstimations } from "./player-estimations.js";
+import { userEvent } from "@testing-library/user-event";
 
 afterEach(cleanup);
 
@@ -28,4 +29,34 @@ test("all estimated", () => {
   const { container } = render(<PlayerEstimations total={5} estimations={estimations} />);
 
   expect(container).toMatchSnapshot();
+});
+
+test("disable button if can not reveal", () => {
+  const estimations = Array.from(new Array(5)).map((_, index) => {
+    return { name: `foo${index}`, estimated: true };
+  });
+
+  const { container } = render(<PlayerEstimations total={5} estimations={estimations} />);
+
+  expect(container).toMatchSnapshot();
+});
+
+test("call onReveal if it is revealable", async () => {
+  expect.assertions(1);
+  const estimations = Array.from(new Array(5)).map((_, index) => {
+    return { name: `foo${index}`, estimated: true };
+  });
+
+  render(
+    <PlayerEstimations
+      total={5}
+      estimations={estimations}
+      revealable
+      onReveal={() => {
+        expect(true).toBeTruthy();
+      }}
+    />
+  );
+
+  await userEvent.click(screen.getByRole("button"));
 });
--- features/voting/src/components/presentations/player-estimations.tsx
+++ features/voting/src/components/presentations/player-estimations.tsx
@@ -3,22 +3,26 @@ import { Skeleton } from "@spp/ui-skeleton";
 import { useMemo } from "react";
 import { EstimationDto } from "../../atoms/dto.js";
 import { PlayerEstimation } from "./player-estimation.js";
+import { buttonStyle } from "@spp/ui-button-style";
+import { Variant } from "@spp/shared-color-variant";
 
 interface Props {
   loading?: boolean;
   total: number;
   estimations: EstimationDto[];
+  revealable?: boolean;
+  onReveal?: () => void;
 }
 
 const styles = {
-  root: clsx("grid", "grid-rows-[auto_1fr]", "grid-cols-1", "place-items-center", "gap-4"),
+  root: clsx("grid", "grid-rows-[auto_auto_1fr]", "grid-cols-1", "place-items-center", "gap-4"),
   loading: clsx("grid", "grid-rows-1", "grid-cols-1", "place-content-center", "h-24", "w-full"),
   estimations: clsx("flex", "flex-row", "gap-4"),
   voting: {
-    root: clsx("flex", "flex-row", "items-center", "justify-center", "h-8"),
+    root: clsx("flex", "flex-row", "items-center", "justify-center", "h-16"),
     label: clsx("text-2xl", "font-bold", "text-emerald-700", "mr-4", "flex-none"),
     estimated: (allEstimated: boolean) =>
-      clsx("flex-auto", "text-xl", "rounded", "px-2", "py-1", "border", "transition", {
+      clsx("flex-auto", "text-xl", "rounded", "px-4", "py-2", "border", "transition", {
         "text-gray-800": !allEstimated,
         "border-gray-400": !allEstimated,
         "text-emerald-700": allEstimated,
@@ -26,6 +30,8 @@ const styles = {
         "bg-cerise-100": allEstimated,
         "border-cerise-400": allEstimated,
       }),
+    revealButton: (revealable: boolean) => clsx(buttonStyle({ variant: Variant.emerald, disabled: !revealable })),
+    reveal: clsx("flex", "flex-auto", "ml-4"),
   },
 } as const;
 
@@ -33,7 +39,7 @@ const styles = {
  * Container presentation for player estimations
  */
 // eslint-disable-next-line func-style
-export function PlayerEstimations({ loading, total, estimations }: Props) {
+export function PlayerEstimations({ loading, total, estimations, onReveal, revealable = false }: Props) {
   const estimated = useMemo(() => estimations.filter((v) => v.estimated).length, [estimations]);
 
   if (loading) {
@@ -44,6 +50,10 @@ export function PlayerEstimations({ loading, total, estimations }: Props) {
     );
   }
 
+  const handleReveal = () => {
+    onReveal?.();
+  };
+
   const allEstimated = total == estimated;
 
   return (
@@ -53,6 +63,11 @@ export function PlayerEstimations({ loading, total, estimations }: Props) {
         <span className={styles.voting.estimated(allEstimated)}>
           {estimated} / {total}
         </span>
+      </div>
+      <div className={styles.voting.reveal}>
+        <button className={styles.voting.revealButton(revealable)} onClick={handleReveal} disabled={!revealable}>
+          Reveal
+        </button>
       </div>
       <div className={styles.estimations}>
         {estimations.map((estimation, index) => {
