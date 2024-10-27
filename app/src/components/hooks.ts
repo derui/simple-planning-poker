9c93e2a0f73dca368bba8d2220efb835907f3014
derui <derutakayu@gmail.com>
2024-10-27 10:32:53 +0900

    refactor: delete files that are moved to other packages

--- app/src/components/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, RootState } from "@/status/store";
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- app/src/domains/base.ts
+++
@@ -1,9 +0,0 @@
-import { v4 } from "uuid";
-
-// generic ID type
-export type Id<T extends string> = string & { [key in T]: never };
-
-export const toString = <T extends string>(v: Id<T>): string => v as string;
-
-export const create = <T extends string>(constant: string | undefined = undefined): Id<T> =>
-  (constant || v4()) as Id<T>;
