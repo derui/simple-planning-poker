693c3716d7aa67f9d3b0e7a445cc10025f51eaae
derui <derutakayu@gmail.com>
2024-10-30 08:04:11 +0900

    fix: use workspace package

--- app/src/routes/layout.tsx
+++ app/src/routes/layout.tsx
@@ -1,6 +1,6 @@
+import { themeClass } from "@spp/ui-theme";
 import { Suspense } from "react";
 import { Outlet } from "react-router";
-import { themeClass } from "../../../packages/ui/theme/dist/style.css.js";
 
 // eslint-disable-next-line func-style
 export function RootLayout() {
