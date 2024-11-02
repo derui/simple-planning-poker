2ce26b899d9edc05956c4e7a5b976d5c2532add6
derui <derutakayu@gmail.com>
2024-11-02 13:57:43 +0900

    fix: export from module

--- packages/shared-logic/use-case/src/index.ts
+++ packages/shared-logic/use-case/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./base.js";
 export * from "./change-theme.js";
 export * from "./change-user-mode.js";
 export * from "./change-user-name.js";
@@ -5,4 +6,5 @@ export * from "./create-game.js";
 export * from "./estimate-player.js";
 export * from "./reset-voting.js";
 export * from "./reveal.js";
-export * from "./base.js";
+export { newStartVotingUseCase } from "./start-voting.js";
+export type { StartVotingUseCase, StartVotingUseCaseInput, StartVotingUseCaseOutput } from "./start-voting.js";
