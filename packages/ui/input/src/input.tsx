0c45e4f3a5bb04a80ad4e4672691e47cf007117e
derui <derutakayu@gmail.com>
2024-11-04 15:41:08 +0900

    fix: use forwardRef

--- packages/ui/input/src/input.tsx
+++ packages/ui/input/src/input.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 import { input } from "./style.css.js";
 
-export const Input = function Input(props: React.HTMLProps<HTMLInputElement>): JSX.Element {
+const _Input = function Input(props: React.HTMLProps<HTMLInputElement>, ref: React.Ref<HTMLInputElement> | undefined) {
   const { className: _, ...rest } = props;
-  return <input {...rest} className={input} />;
+  return <input ref={ref} {...rest} className={input} />;
 };
+
+export const Input: React.ForwardRefExoticComponent<
+  Omit<React.HTMLProps<HTMLInputElement>, "ref"> & React.RefAttributes<HTMLInputElement>
+> = React.forwardRef(_Input);
