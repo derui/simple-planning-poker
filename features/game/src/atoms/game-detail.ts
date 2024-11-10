e64b9bcbbaf5874b77a6b170b33e56cb2093f850
derui <derutakayu@gmail.com>
2024-11-10 10:44:24 +0900

    feat: implement edit with use case

--- features/game/src/atoms/game-detail.ts
+++ features/game/src/atoms/game-detail.ts
@@ -1,7 +1,10 @@
-import { GameRepository } from "@spp/shared-domain";
-import { atom, useAtom, useAtomValue } from "jotai";
+import { UseLoginUser } from "@spp/feature-login";
+import { GameRepository, User } from "@spp/shared-domain";
+import { EditGameUseCase, EditGameUseCaseInput } from "@spp/shared-use-case";
+import { atom, useAtom } from "jotai";
+import { useSetAtom } from "jotai/ts3.8/esm/react";
 import { GameDto, toGameDto } from "./dto.js";
-import { selectedGameAtom } from "./game-atom.js";
+import { gamesAtom, selectedGameAtom } from "./game-atom.js";
 
 const statusAtom = atom<"editing" | "completed">("completed");
 
@@ -35,21 +38,56 @@ export type UseGameDetail = () => {
  */
 export const useGameDetail = function useGameDetail({
   gameRepository,
+  useLoginUser,
+  editGameUseCase,
 }: {
   gameRepository: GameRepository.T;
+  useLoginUser: UseLoginUser;
+  editGameUseCase: EditGameUseCase;
 }): UseGameDetail {
   return () => {
     const [status, setStatus] = useAtom(statusAtom);
-    const game = useAtomValue(selectedGameAtom);
+    const { userId } = useLoginUser();
+    const [game, setGame] = useAtom(selectedGameAtom);
+    const setGames = useSetAtom(gamesAtom);
 
     return {
       loading: status == "editing",
       game: game ? toGameDto(game) : undefined,
 
       edit: (name: string, points: string): void => {
-        if (!game) {
+        if (!game || !userId) {
           return;
         }
+
+        const input: EditGameUseCaseInput = {
+          gameId: game.id,
+          name,
+          points: points.split(",").map((v) => parseInt(v)),
+          ownedBy: userId,
+        };
+
+        async function _do() {
+          try {
+            const ret = await editGameUseCase(input);
+
+            switch (ret.kind) {
+              case "success":
+                setGame(ret.game);
+                setGames(await gameRepository.listUserCreated(User.createId(userId)));
+                break;
+              default:
+                break;
+            }
+          } catch (err) {
+            console.error(err);
+          } finally {
+            setStatus("completed");
+          }
+        }
+
+        setStatus("editing");
+        _do();
       },
 
       delete: () => {
