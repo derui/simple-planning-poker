a1c6d59121273312a8bfa2aad8e440cfbc63fa03
derui <derutakayu@gmail.com>
2025-01-13 11:07:51 +0900

    style: Remove unused font size and weight from theme editor header

--- features/voting/src/components/presentations/theme-editor.css.ts
+++ features/voting/src/components/presentations/theme-editor.css.ts
@@ -74,7 +74,5 @@ export const cancelButtonHidden: string = style([
 export const header: string = style({
   padding: `${vars.spacing[2]} ${vars.spacing[4]}`,
   margin: vars.spacing[1],
-  fontSize: vars.fontSize[5],
-  fontWeight: vars.fontWeight.bold,
   borderBottom: `1px solid ${vars.color.gray[300]}`,
 });
