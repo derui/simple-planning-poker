c9cceade1ca25d4e18943eadcdff18c022d9018f
derui <derutakayu@gmail.com>
2025-01-19 13:36:56 +0900

    fix: return 0 if on estimation

--- features/voting/src/atoms/use-revealed.ts
+++ features/voting/src/atoms/use-revealed.ts
@@ -47,12 +47,12 @@ export const useRevealed: UseRevealed = function useRevealed() {
     }
 
     if (data.estimations && data.estimations.length > 0) {
-      const filteredEstimations = data.estimations.filter(estimation => estimation.estimated);
+      const filteredEstimations = data.estimations.filter((estimation) => estimation.estimated);
       if (filteredEstimations.length === 0) {
-        return undefined;
+        return 0;
       }
 
-      const total = filteredEstimations.reduce((sum, estimation) => sum + Number(estimation.estimated ?? "0"), 0);
+      const total = filteredEstimations.reduce((sum, estimation) => sum + Number(estimation.estimated!), 0);
       const average = total / filteredEstimations.length;
 
       return parseFloat(average.toFixed(1));
